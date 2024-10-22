d0de13f3d75a760839b2741edbf6746de065fb6d
Marius Räsener <931919+elmcrest@users.noreply.github.com>
2024-10-22 11:12:00 +0200

    good bye fastapi, hello starlette

--- src/ots/main.py
+++ src/ots/main.py
@@ -1,60 +1,58 @@
 from secrets import token_urlsafe
 
-from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.routing import Mount, Route
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="src/static"), name="static")
 templates = Jinja2Templates(directory="src/templates")
 
 # Use a simple dictionary to store secrets
 app_state = {}
 
 
-@app.post("/secret/new/")
-async def secret_new(secret: str = Form()):
-    if secret:
-        share_token = token_urlsafe(6)
-        app_state[share_token] = secret
-        return RedirectResponse(url=f"/secret/share/{share_token}", status_code=303)
-    return {"secret": secret}
+async def secret_new(request: Request):
+    async with request.form() as form:
+        if form["secret"]:
+            share_token = token_urlsafe(6)
+            app_state[share_token] = form["secret"]
+            return RedirectResponse(
+                url=request.url_for("secret_share", share_token=share_token),
+                status_code=303,
+            )
+    return RedirectResponse(url="/")
 
 
-@app.get("/secret/share/{share_token}", response_class=HTMLResponse)
 def secret_share(
     request: Request,
-    share_token: str,
 ):
     return templates.TemplateResponse(
         name="share.html",
         context={
             "request": request,
-            "share_token": share_token,
+            "share_token": request.path_params["share_token"],
         },
     )
 
 
-@app.get("/secret/{share_token}", response_class=HTMLResponse)
 def secret_preview(
     request: Request,
-    share_token: str,
 ):
     return templates.TemplateResponse(
         name="preview.html",
         context={
             "request": request,
-            "share_token": share_token,
+            "share_token": request.path_params["share_token"],
         },
     )
 
 
-@app.get("/secret/receive/{share_token}", response_class=HTMLResponse)
 def secret_receive(
     request: Request,
-    share_token: str,
 ):
+    share_token = request.path_params["share_token"]
     # Retrieve and remove the secret from the dictionary
     secret = app_state.pop(share_token, None)
     if secret:
@@ -75,7 +73,6 @@ def secret_receive(
         )
 
 
-@app.get("/", response_class=HTMLResponse)
 async def index(
     request: Request,
 ):
@@ -85,3 +82,14 @@ async def index(
             "request": request,
         },
     )
+
+
+routes = [
+    Route("/", index),
+    Route("/secret/receive/{share_token}", secret_receive),
+    Route("/secret/{share_token}", secret_preview),
+    Route("/secret/share/{share_token}", secret_share),
+    Route("/secret/new/", secret_new, methods=["POST"]),
+    Mount("/static", StaticFiles(directory="src/static"), name="static"),
+]
+app = Starlette(debug=True, routes=routes)
